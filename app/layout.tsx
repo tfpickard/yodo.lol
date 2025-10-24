856ec4c34681b04aeb5fb170b69b2787495079e8
Tom Pickard <tom@pickard.dev>
2025-10-24 12:18:13 -0600

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: 'YODO.LOL - AI-Powered Chaotic Reddit Feed',
